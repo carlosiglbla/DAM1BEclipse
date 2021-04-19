eb04963597e3680ab96b6a82d813baa1aaaeda84
Usu2 name <usu2@example.com>
2021-04-19 16:59:15 +0200

    Validando cambio2 en prueba1

---
+++ prueba1/src/cambio1/Cambio2.java
@@ -0,0 +1,10 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Estoy en prueba1");
+		System.out.println("Nueva línea en cambio2 de prueba1");
+	}
+
+}
