bbd494e4b08800baa4d031fa459bbdfbb5f0d23c
Usu2 name <usu2@example.com>
2021-04-19 16:35:36 +0200

    Validación en cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo. Probando Git-Eclipse");
 		System.out.println("Hola otra vez.");
+		System.out.println("Hola Mundo desde la rama Cambio1");
 	}
 
 }
